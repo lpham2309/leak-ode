a2938b149f42cfacf1384b4f456eec09e8b4fdfe
Lam Pham <clbt.pham3110@gmail.com>
2024-07-30 09:50:51 -0400

    add invert binary tree

--- src/InvertBinaryTree/InvertBinaryTree.py
+++ src/InvertBinaryTree/InvertBinaryTree.py
@@ -3,4 +3,14 @@ from src.models.TreeNode import TreeNode
 
 class InvertBinaryTree():
     def invertTree(self, root: Optional[TreeNode]) -> bool:
-        pass
+        if not root:
+           return root
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        return root 
