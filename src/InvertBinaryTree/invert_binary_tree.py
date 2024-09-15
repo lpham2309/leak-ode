c127da254ff90fe7621321df25fb284c2339edeb
Lam Pham <clbt.pham3110@gmail.com>
2024-09-15 16:56:33 -0400

    add iterative solution for Invert Binary Tree

--- src/InvertBinaryTree/invert_binary_tree.py
+++ src/InvertBinaryTree/invert_binary_tree.py
@@ -7,6 +7,9 @@ from typing import Optional
 from src.models.TreeNode import TreeNode
 
 class InvertBinaryTree:
+    """
+    Recursive Solution
+    """
     def invertTree(self, root: Optional[TreeNode]) -> Optional:
         if not root:
            return root
@@ -18,4 +21,18 @@ class InvertBinaryTree:
         root.left = root.right
         root.right = temp
 
-        return root 
+        return root
+    """
+    Iterative Solution
+    """
+    def invertTreeIterative(self, root: Optional[TreeNode]) -> Optional:
+        if root:
+            queue = [root]
+
+            while queue:
+                curr_node = queue.pop(0)
+                if curr_node.left:
+                    queue.append(curr_node.left)
+                elif curr_node.right:
+                    queue.append(curr_node.right)
+        return root
